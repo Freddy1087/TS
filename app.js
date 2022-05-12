cf8a7a7f16b43824e1e6b0bfc9ac4764bcdc539a
Freddy Mejia <100593377+Freddy1087@users.noreply.github.com>
2022-05-12 11:42:22 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,21 +2,30 @@
 //   name: string;
 //   age: number;
 // } = {
+// const person: {      // Course 19
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
 var person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["sports", "cooking"],
-  role: [2, "author"],
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["sports", "cooking"],
+    role: [2, "author"]
 };
-person.role.push("admin");
+// person.role.push("admin");
 // person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ["gaming", "driving"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-  var hobby = _a[_i];
-  console.log(hobby.toUpperCase());
-  //   console.log(hobby.map()); // !!! ERROR !!!
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    //   console.log(hobby.map()); // !!! ERROR !!!
 }
-
-// console.log(person.role);
